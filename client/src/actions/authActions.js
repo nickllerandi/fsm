98feaf67af4b8442b8e62e26a7fd566831484c20
nicholas llerandi <nicholasllerandi@gmail.com>
2019-02-02 09:34:41 -0500

    isAuthenticated-true-is-not-persisting-on-page-load-#23

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
+import {setAuthToken} from "../utils/setAuthToken";
+
 import {GET_ERRORS, SET_CURRENT_USER} from "./types";
 
 export const registerUser = (userData, history) => dispatch => {
@@ -21,7 +23,7 @@ export const loginUser = userData => dispatch => {
             localStorage.setItem('jwtToken', token);
             setAuthToken(token);
             const decoded = jwtDecode(token);
-            // REFACTOR ME
+            // REFACTOR ME?
             dispatch(setCurrentUser(decoded))
         })
         .catch(err =>
@@ -32,17 +34,9 @@ export const loginUser = userData => dispatch => {
         )
 };
 
-// MOVE ME TO SRC/UTILS
-const setAuthToken = token => {
-    if (token) {
-        axios.defaults.headers.common["Authorization"] = token;
-    } else {
-        delete axios.defaults.headers.common["Authorization"];
-    }
-};
 
 // SHOULD I GO SOMEWHERE?
-const setCurrentUser = (decoded) => {
+export const setCurrentUser = (decoded) => {
     return {
         type: SET_CURRENT_USER,
         payload: decoded
