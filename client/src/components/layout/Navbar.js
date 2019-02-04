30ca77321a275a62ebe5dffdb674ba58d5805a09
nicholas llerandi <nicholasllerandi@gmail.com>
2019-02-04 10:42:52 -0500

    nllerandi/logout-func#26

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,17 +1,49 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+
+import {logoutUser} from "../../actions/authActions";
 
 class Navbar extends Component {
+    onLogoutClick = (e) => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
+
     render() {
+        const {isAuthenticated} = this.props.authReducer;
+
+        const outState = (
+            <div>
+                <Link to="/login">Login</Link> -
+                <Link to="/register">Signup</Link>
+            </div>
+        );
+
+        const inState = (
+            <div>
+                <a
+                    href="#"
+                    onClick={this.onLogoutClick}
+                >
+                    Logout
+                </a>
+            </div>
+        );
+
+
         return (
             <nav className="Navbar">
                 <Link to="/">FSM</Link> - Search -
-                <Link to="/tags">Tags</Link> -
-                <Link to="/login">Login</Link> -
-                <Link to="/register">Signup</Link>
+                <Link to="/tags">Tags</Link>
+                {isAuthenticated ? inState : outState}
             </nav>
         )
     }
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+    authReducer: state.authReducer
+});
+
+export default connect(mapStateToProps, {logoutUser})(Navbar);
