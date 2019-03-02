6325ecf8346f8290421dfbaa5f88f0724fe8f76f
nicholas llerandi <nicholasllerandi@gmail.com>
2019-03-02 11:04:51 -0500

    /edit private route

--- client/src/components/profiles/ProfileEdit.js
+++ client/src/components/profiles/ProfileEdit.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {Component} from "react";
 import {connect} from "react-redux";
 
-const ProfileEdit = () => {
-    return (
-        <div>
-            Profile Edit
-        </div>
-    );
-};
+class ProfileEdit extends Component {
+    componentDidMount() {
+        if (this.props.match.params.id !== this.props.authReducer.user.id) {
+            this.props.history.push("/");
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Profile Edit
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = state => ({
     authReducer: state.authReducer
