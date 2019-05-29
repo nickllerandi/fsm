d77ff3a5781c36403f36ec5c55694bd794dc29f7
nicholas llerandi <nicholasllerandi@gmail.com>
2019-05-29 09:05:16 -0400

    sidebar flex column

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,38 +1,77 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
 
 // ASSETS
 import sprites from '../../img/sprite.svg'
 
+// UTILS
+import {white} from '../../utils'
+
 const Sidebar = () => {
     return (
-        <ul className='side-nav'>
-            <li className='side-nav__item'>
-                <Link to='/'>
-                    <svg className='side-nav__icon'>
-                        <use xlinkHref={`${sprites}#icon-home`} />
-                    </svg>
-                    <span>Home</span>
-                </Link>
-            </li>
-            <li className='side-nav__item'>
-                <Link to='/'>
-                    <svg className='side-nav__icon'>
-                        <use xlinkHref={`${sprites}#icon-price-tag`} />
-                    </svg>
-                    <span>Tags</span>
-                </Link>
-            </li>
-            <li className='side-nav__item'>
-                <Link to='/'>
-                    <svg className='side-nav__icon'>
-                        <use xlinkHref={`${sprites}#icon-slideshare`} />
-                    </svg>
-                    <span>Users</span>
-                </Link>
-            </li>
-        </ul>
+        <SidebarStyled>
+            <ul className='side-nav'>
+                <li className='side-nav__item'>
+                    <Link to='/' className='side-nav__link'>
+                        <svg className='side-nav__icon'>
+                            <use xlinkHref={`${sprites}#icon-home`} />
+                        </svg>
+                        <span>Home</span>
+                    </Link>
+                </li>
+                <li className='side-nav__item'>
+                    <Link to='/' className='side-nav__link'>
+                        <svg className='side-nav__icon'>
+                            <use xlinkHref={`${sprites}#icon-price-tag`} />
+                        </svg>
+                        <span>Tags</span>
+                    </Link>
+                </li>
+                <li className='side-nav__item'>
+                    <Link to='/' className='side-nav__link'>
+                        <svg className='side-nav__icon'>
+                            <use xlinkHref={`${sprites}#icon-slideshare`} />
+                        </svg>
+                        <span>Users</span>
+                    </Link>
+                </li>
+            </ul>
+        
+            <div className='legal'>
+                &copy; 2019 by Fullstack Musician.
+            </div>
+        </SidebarStyled>
     );
 }
+
+const SidebarStyled = styled.div`   
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    .side-nav {
+        &__item {
+
+        }
+
+        &__link {
+
+        }
+
+        &__icon {
+            width: 1.75rem;
+            height: 1.75rem;
+        }
+    }
+
+    .legal {
+        font-size: 1.2rem;
+        text-align: center;
+        padding: 2.5rem;
+        color: ${white};
+    }
+`
 
 export default Sidebar;
